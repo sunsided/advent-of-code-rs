5d2d21c3b90ae534aef8671c272be7293f49b096
Markus Mayer <widemeadows@gmail.com>
2023-12-05 00:24:13 +0100

    Add solution for 2023, day 4, part 2

--- 2023/day-4/example/main.rs
+++ 2023/day-4/example/main.rs
@@ -4,8 +4,11 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     println!("Day 4: Scratchcards");
+
+    let cards = Card::parse_all(INPUT).expect("invalid input");
+    println!("Total points: {}", Card::sum_all_scores(&cards));
     println!(
-        "Total points: {}",
-        Card::sum_all_scores(INPUT).expect("invalid input")
-    )
+        "Total count of copied cards: {}",
+        Card::count_copied_cards(cards)
+    );
 }
--- 2023/day-4/src/lib.rs
+++ 2023/day-4/src/lib.rs
@@ -4,6 +4,7 @@ use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
 #[allow(dead_code)]
+#[derive(Debug, Clone)]
 pub struct Card {
     card_no: u32,
     winning_numbers: Vec<u32>,
@@ -11,27 +12,60 @@ pub struct Card {
 }
 
 impl Card {
+    /// Parses all lines into a vector of [`Card`].
     pub fn parse_all(input: &str) -> Result<Vec<Card>, ParseCardError> {
         input
             .lines()
             .map(|line| line.trim())
             .filter(|line| !line.is_empty())
-            .map(|line| Card::from_str(line))
+            .map(Card::from_str)
             .collect()
     }
 
-    pub fn sum_all_scores(input: &str) -> Result<u32, ParseCardError> {
-        Ok(Self::parse_all(input)?
-            .iter()
-            .fold(0, |sum, card| sum + card.get_score()))
+    /// Sums all winning scores across all cards..
+    pub fn sum_all_scores<'a, C: IntoIterator<Item = &'a Card>>(cards: C) -> u32 {
+        cards
+            .into_iter()
+            .fold(0, |sum, card| sum + card.get_score())
     }
 
-    pub fn get_num_winning(&self) -> u32 {
+    /// Counts the number of copied cards.
+    pub fn count_copied_cards<C: IntoIterator<Item = Card>>(cards: C) -> u32 {
+        Self::determine_copies(cards)
+            .iter()
+            .fold(0, |sum, card| sum + card.0)
+    }
+
+    /// Determines the number of copies per card.
+    pub fn determine_copies<C: IntoIterator<Item = Card>>(cards: C) -> Vec<(u32, Card)> {
+        let mut cards: Vec<_> = cards.into_iter().map(|c| (1, c)).collect();
+        for i in 0..cards.len() {
+            let num_copies_to_make = cards[i].0;
+            let num_rows_to_copy = cards[i].1.get_num_winning() as usize;
+            for (count, _) in cards.iter_mut().take(i + num_rows_to_copy + 1).skip(i + 1) {
+                *count += num_copies_to_make;
+            }
+        }
+
+        cards
+    }
+
+    /// Returns the number of winning numbers in `our_numbers`.
+    fn get_num_winning(&self) -> u32 {
         let winning: HashSet<&u32> = HashSet::from_iter(&self.winning_numbers);
         let ours = HashSet::from_iter(&self.our_numbers);
         winning.intersection(&ours).count() as u32
     }
 
+    /// Calculate the score based on the number of winnings.
+    ///
+    /// # Arguments
+    ///
+    /// - `self` - The reference to the object.
+    ///
+    /// # Returns
+    ///
+    /// The score calculated based on the number of winnings.
     pub fn get_score(&self) -> u32 {
         let our_winning = self.get_num_winning();
         if our_winning > 0 {
@@ -68,13 +102,13 @@ impl FromStr for Card {
 
         let winning_numbers = winning_numbers
             .split_whitespace()
-            .map(|s| u32::from_str(s))
+            .map(u32::from_str)
             .collect::<Result<_, _>>()
             .map_err(|_| ParseCardError("failed to parse a winning number"))?;
 
         let our_numbers = our_numbers
             .split_whitespace()
-            .map(|s| u32::from_str(s))
+            .map(u32::from_str)
             .collect::<Result<_, _>>()
             .map_err(|_| ParseCardError("failed to parse an owned number"))?;
 
@@ -126,5 +160,28 @@ mod tests {
         let card = Card::from_str(input).expect("invalid card");
         assert_eq!(card.get_num_winning(), num_winning);
         assert_eq!(card.get_score(), score);
+    }
+
+    #[test]
+    fn test_count_copies() {
+        const INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                             Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                             Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                             Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                             Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                             Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        let cards = Card::parse_all(INPUT).expect("invalid input");
+        let counted = Card::determine_copies(cards.clone());
+        assert_eq!(counted.len(), 6);
+        assert_eq!(counted[0].0, 1);
+        assert_eq!(counted[1].0, 1 + 1);
+        assert_eq!(counted[2].0, 1 + 1 + 2);
+        assert_eq!(counted[3].0, 1 + 1 + 2 + 4);
+        assert_eq!(counted[4].0, 1 + 1 + 4 + 8);
+        assert_eq!(counted[5].0, 1);
+
+        let total_copies = Card::count_copied_cards(cards);
+        assert_eq!(total_copies, 30);
     }
 }
