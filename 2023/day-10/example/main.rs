638ec17c18f670f77667df1666be2b60d464c6a8
Markus Mayer <widemeadows@gmail.com>
2023-12-10 12:55:31 +0100

    Add 2023 day 10, part 1

---
+++ 2023/day-10/example/main.rs
@@ -0,0 +1,9 @@
+use aoc_2023_day_10::*;
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    println!("2023 Day 10: Pipe Maze");
+    println!("...: {}", part1(INPUT));
+    println!("...: {}", part2(INPUT));
+}
---
+++ 2023/day-10/src/lib.rs
@@ -0,0 +1,30 @@
+/// Solution for part 1.
+pub fn part1(input: &str) -> i64 {
+    todo!()
+}
+
+/// Solution for part 2.
+pub fn part2(input: &str) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn test_part1() {
+        const TEST: &str = "...";
+
+        assert_eq!(part1(TEST), todo!());
+    }
+
+    #[test]
+    fn test_part2() {
+        const TEST: &str = "...";
+
+        assert_eq!(part2(TEST), todo!());
+    }
+}
