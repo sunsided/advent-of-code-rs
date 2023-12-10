06c57163f885d373b8192dbe42725c4a35f5416c
Markus Mayer <widemeadows@gmail.com>
2023-12-10 12:44:30 +0100

    Add solution for 2023 day 9, part 2

--- 2023/day-9/example/main.rs
+++ 2023/day-9/example/main.rs
@@ -4,5 +4,12 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     println!("2023 Day 9: Mirage Maintenance");
-    println!("The sum of all history predictions is: {}", part1(INPUT));
+    println!(
+        "The sum of all (next) history predictions is: {}",
+        part1(INPUT)
+    );
+    println!(
+        "The sum of all (previous) history predictions is: {}",
+        part2(INPUT)
+    );
 }
--- 2023/day-9/src/lib.rs
+++ 2023/day-9/src/lib.rs
@@ -12,6 +12,17 @@ pub fn part1(input: &str) -> i64 {
         .sum()
 }
 
+/// Solution for part 2.
+pub fn part2(input: &str) -> i64 {
+    input
+        .lines()
+        .filter(|&line| !line.is_empty())
+        .map(parse_whitespace_delimited::<i64>)
+        .map(|result| result.expect("invalid input"))
+        .map(predict_part2)
+        .sum()
+}
+
 /// Obtains the new history value prediction for part 1.
 fn predict_part1(mut history: Vec<i64>) -> i64 {
     let mut last_values = vec![*history.last().expect("history has zero length")];
@@ -22,6 +33,21 @@ fn predict_part1(mut history: Vec<i64>) -> i64 {
     }
 
     last_values.into_iter().sum()
+}
+
+/// Obtains the new history value prediction for part 2.
+fn predict_part2(mut history: Vec<i64>) -> i64 {
+    let mut last_values = vec![*history.first().expect("history has zero length")];
+
+    while !all_zero(&history) {
+        history = differentiate(&history);
+        last_values.push(*history.first().expect("history has zero length"));
+    }
+
+    last_values
+        .into_iter()
+        .rev()
+        .fold(0, |sum, current| current - sum)
 }
 
 /// Determines whether all input values are zero.
@@ -46,7 +72,7 @@ mod tests {
     const INPUT: &str = include_str!("../input.txt");
 
     #[test]
-    fn test_prediction() {
+    fn test_prediction_part1() {
         assert_eq!(predict_part1(vec![0, 3, 6, 9, 12, 15]), 18);
         assert_eq!(predict_part1(vec![1, 3, 6, 10, 15, 21]), 28);
     }
@@ -58,5 +84,19 @@ mod tests {
             10 13 16 21 30 45";
 
         assert_eq!(part1(TEST), 114);
+    }
+
+    #[test]
+    fn test_prediction_part2() {
+        assert_eq!(predict_part2(vec![10, 13, 16, 21, 30, 45]), 5);
+    }
+
+    #[test]
+    fn test_part2() {
+        const TEST: &str = "0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45";
+
+        assert_eq!(part2(TEST), 2);
     }
 }
