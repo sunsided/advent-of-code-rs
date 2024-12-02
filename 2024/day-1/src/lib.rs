4a969fddbc41df52475abd7fc27f0ea56a6a2e51
Markus Mayer <widemeadows@gmail.com>
2024-12-02 19:32:04 +0100

    March both lists in parallel

--- 2024/day-1/src/lib.rs
+++ 2024/day-1/src/lib.rs
@@ -1,5 +1,4 @@
 use aoc_utils::parse_whitespace_delimited;
-use std::collections::HashMap;
 
 pub fn first_part(input: &str) -> i128 {
     let (lhs, rhs) = split_values(input);
@@ -36,24 +35,54 @@ fn sum_distances(lhs: &[i128], rhs: &[i128]) -> i128 {
         .sum()
 }
 
-fn sum_scores(lhs: &[i128], rhs: &[i128]) -> i128 {
-    let lhs = count_occurrences(lhs);
-    let rhs = count_occurrences(rhs);
+fn sum_scores(sorted_lhs: &[i128], sorted_rhs: &[i128]) -> i128 {
+    let lhs_counts = count_occurrences(sorted_lhs);
+    let rhs_counts = count_occurrences(sorted_rhs);
 
-    lhs.iter()
-        .filter_map(|(num, &lhs_count)| {
-            rhs.get(num)
-                .map(|&rhs_count| *num * lhs_count as i128 * rhs_count as i128)
-        })
-        .sum()
+    // Iterate through both counts in parallel
+    let mut lhs_iter = lhs_counts.into_iter();
+    let mut rhs_iter = rhs_counts.into_iter();
+    let mut lhs_entry = lhs_iter.next();
+    let mut rhs_entry = rhs_iter.next();
+    let mut total_sum = 0;
+
+    while let (Some(lhs_item), Some(rhs_item)) = (lhs_entry.as_ref(), rhs_entry.as_ref()) {
+        match (lhs_item, rhs_item) {
+            (lhs, rhs) if lhs.value == rhs.value => {
+                total_sum += lhs.value * (lhs.count as i128) * (rhs.count as i128);
+                lhs_entry = lhs_iter.next();
+                rhs_entry = rhs_iter.next();
+            }
+            (lhs, rhs) if lhs.value < rhs.value => {
+                lhs_entry = lhs_iter.next();
+            }
+            (lhs, rhs) if lhs.value > rhs.value => {
+                rhs_entry = rhs_iter.next();
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    total_sum
 }
 
-fn count_occurrences(values: &[i128]) -> HashMap<i128, usize> {
-    let mut occurrences = HashMap::new();
-    for &value in values {
-        *occurrences.entry(value).or_insert(0) += 1;
+struct Item {
+    value: i128,
+    count: usize,
+}
+
+fn count_occurrences(sorted_lhs: &[i128]) -> Vec<Item> {
+    let mut counts = Vec::new();
+    let mut current_value = sorted_lhs.first().cloned();
+    for &value in sorted_lhs {
+        if Some(value) != current_value || counts.is_empty() {
+            counts.push(Item { value, count: 1 });
+            current_value = Some(value);
+        } else if let Some(last) = counts.last_mut() {
+            last.count += 1;
+        }
     }
-    occurrences
+    counts
 }
 
 #[cfg(test)]
