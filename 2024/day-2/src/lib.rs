a006df311c64b230ef1d9aa35bbd2897fdf004b2
Markus Mayer <widemeadows@gmail.com>
2024-12-02 21:01:20 +0100

    Add notes for solution of 2024 day 2, part 2

--- 2024/day-2/src/lib.rs
+++ 2024/day-2/src/lib.rs
@@ -1,7 +1,19 @@
 use aoc_utils::parse_whitespace_delimited;
 
 pub fn first_part(input: &str) -> usize {
+    count_safe(input, false)
+}
+
+pub fn second_part(input: &str) -> usize {
+    count_safe(input, true)
+}
+
+fn count_safe(input: &str, allow_single_outtake: bool) -> usize {
     let mut safe = 0;
+    let mut already_found_problem: bool = false;
+
+    // TODO: 1 2 7 8 9 - part 2: removing 2 makes it 1 -> 7, which is unsafe; removing 7 makes it 2 -> 8, which is unsafe
+
     'line: for line in input.trim().lines() {
         let numbers = parse_whitespace_delimited::<usize>(line).expect("failed to parse line");
         let mut iter = numbers
@@ -12,13 +24,21 @@ pub fn first_part(input: &str) -> usize {
         // Check start condition
         match iter.next().expect("expected at least two numbers") {
             0 => {
-                continue 'line;
+                if !allow_single_outtake || already_found_problem {
+                    continue 'line;
+                } else {
+                    already_found_problem = true;
+                }
             }
             x if (-3..0).contains(&x) => {
                 // Ensure all descending.
                 for x in iter {
                     if !(-3..0).contains(&x) {
-                        continue 'line;
+                        if !allow_single_outtake || already_found_problem {
+                            continue 'line;
+                        } else {
+                            already_found_problem = true;
+                        }
                     }
                 }
             }
@@ -26,12 +46,20 @@ pub fn first_part(input: &str) -> usize {
                 // Ensure all ascending.
                 for x in iter {
                     if x <= 0 || x > 3 {
-                        continue 'line;
+                        if !allow_single_outtake || already_found_problem {
+                            continue 'line;
+                        } else {
+                            already_found_problem = true;
+                        }
                     }
                 }
             }
             _ => {
-                continue 'line;
+                if !allow_single_outtake || already_found_problem {
+                    continue 'line;
+                } else {
+                    already_found_problem = true;
+                }
             }
         }
 
@@ -57,5 +85,10 @@ mod tests {
     #[test]
     fn test_first_part() {
         assert_eq!(first_part(INPUT), 2);
+    }
+
+    #[test]
+    fn test_second_part() {
+        assert_eq!(second_part(INPUT), 4);
     }
 }
